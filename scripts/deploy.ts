521407857042971933f0ec7ad8c81defd5e04644
glinda93 <72871001+glinda93@users.noreply.github.com>
2022-04-02 14:46:30 +0800

    fix deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,7 +4,7 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
-import config from "../../deploy.config.json";
+import config from "../deploy.config.json";
 
 enum MarketStatus {
   NONE,
